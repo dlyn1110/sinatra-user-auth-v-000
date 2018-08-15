591ac94d7ea2bee0ed0d9477a564c8efbaff85f6
Dani Lyn <dlyn1110@gmail.com>
2018-08-15 19:52:26 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "users", force: cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password"
